06ff1ed7336824fe7d5e819f1ef92867ed29e43d
Todd Brunia <tbrunia@gmail.com>
2018-05-09 20:52:29 -0500

    tested todos reducer

--- test/unit/frontend/reducers/todos.spec.js
+++ test/unit/frontend/reducers/todos.spec.js
@@ -1,95 +1,97 @@
+import Chance from 'chance';
+
 import todos from '../../../../src/frontend/reducers/todos';
 
+const chance = new Chance();
+
+const buildRandomTodos = () => chance.n(() => ({
+    [chance.string()]: chance.string()
+}), chance.integer({
+    max: 100,
+    min: 0
+}));
+
 describe('todos reducer', () => {
+    let state,
+        todo;
+
+    beforeEach(() => {
+        state = buildRandomTodos();
+        todo = {
+            [chance.string()]: chance.string(),
+            id: chance.string()
+        };
+    });
+
     test('should handle initial state', () => {
-        expect(
-            todos(undefined, {})
-        ).toEqual([]);
+        expect(todos(undefined, {})).toEqual([]);
     });
 
     test('should handle ADD_TODO', () => {
-        expect(
-            todos([], {
-                todo: {
-                    text: 'Run the tests'
-                },
-                type: 'ADD_TODO'
-            })
-        ).toEqual([
-            {
-                text: 'Run the tests'
-            }
-        ]);
-
-        expect(
-            todos([
-                {
-                    text: 'Run the tests'
-                }
-            ], {
-                todo: {
-                    text: 'Use Redux'
-                },
-                type: 'ADD_TODO'
-            })
-        ).toEqual([
-            {
-                text: 'Run the tests'
-            }, {
-                text: 'Use Redux'
-            }
-        ]);
-
-        expect(
-            todos([
-                {
-                    text: 'Run the tests'
-                }, {
-                    text: 'Use Redux'
-                }
-            ], {
-                todo: {
-                    text: 'Fix the tests'
-                },
-                type: 'ADD_TODO'
-            })
-        ).toEqual([
-            {
-                text: 'Run the tests'
-            }, {
-                text: 'Use Redux'
-            }, {
-                text: 'Fix the tests'
-            }
+        expect(todos(state, {
+            todo,
+            type: 'ADD_TODO'
+        })).toEqual([
+            ...state,
+            todo
         ]);
     });
 
     test('should handle TOGGLE_TODO', () => {
-        expect(
-            todos([
-                {
-                    completed: false,
-                    id: 1,
-                    text: 'Run the tests'
-                }, {
-                    completed: false,
-                    id: 0,
-                    text: 'Use Redux'
+        const id = chance.string();
+
+        state.push({
+            [chance.string()]: chance.string(),
+            id
+        });
+        state.push({
+            [chance.string()]: chance.string(),
+            id: chance.string()
+        });
+
+        expect(todos(state, {
+            id,
+            type: 'TOGGLE_TODO'
+        })).toEqual(state.map((_todo) =>
+            _todo.id === id
+                ? {
+                    ..._todo,
+                    completed: !_todo.completed
                 }
-            ], {
-                id: 1,
-                type: 'TOGGLE_TODO'
-            })
-        ).toEqual([
-            {
-                completed: true,
-                id: 1,
-                text: 'Run the tests'
-            }, {
-                completed: false,
-                id: 0,
-                text: 'Use Redux'
-            }
-        ]);
+                : _todo
+        ));
+    });
+
+    test('should handle DELETE_TODO', () => {
+        const id = chance.string();
+
+        state.push({
+            [chance.string()]: chance.string(),
+            id
+        });
+        state.push({
+            [chance.string()]: chance.string(),
+            id: chance.string()
+        });
+
+        expect(todos(state, {
+            id,
+            type: 'DELETE_TODO'
+        })).toEqual(state.filter((_todo) => _todo.id !== id));
+    });
+
+    test('should handle FETCH_TODOS_SUCCESS', () => {
+        const _todos = buildRandomTodos();
+
+        expect(todos(state, {
+            todos: _todos,
+            type: 'FETCH_TODOS_SUCCESS'
+        })).toEqual(_todos);
+    });
+
+    test('should handle SIGN_OUT', () => {
+        expect(todos(state, {
+            type: 'SIGN_OUT'
+        })).toEqual([]);
     });
 });
