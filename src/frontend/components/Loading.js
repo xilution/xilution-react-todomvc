efbd403bdaf7aa6926164746a4f2a501f3e110b4
Cory Hanson <chanson5000@gmail.com>
2018-06-02 18:18:00 +0000

    Removed some variable usage

--- src/frontend/components/Loading.js
+++ src/frontend/components/Loading.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
 
-const type = "spinningBubbles";
-const color = "#ff0000";
 const loaderStyle = {
+    left: '50%',
     margin: '0',
     position: 'fixed',
-    left: '50%',
     top: '50%',
-    zIndex: '500',
-    transform: 'translate(-50%, -50%)'
+    transform: 'translate(-50%, -50%)',
+    zIndex: '500'
 };
 
 const LoadingIndicator = () => (
     <div style={loaderStyle}>
-    <ReactLoading
-        type={type}
-        color={color}
-        height={100}
-        width={100}
-    />
+        <ReactLoading
+            type={'spinningBubbles'}
+            color={'#ff0000'}
+            height={100}
+            width={100}
+        />
     </div>
 );
 
